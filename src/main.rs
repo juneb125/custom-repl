5ee1e5de77c625c40fdeba8e1770534113a9afbe
June B <jrbergeron823@gmail.com>
2025-02-02 00:03:37 -0600

    Implement basic shell-like sub-env (w/ a CL prompt), prepare for special/reserved cmd's

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Result as IOResult, Write};
+
+fn main() -> IOResult<()> {
+    println!("Hello, world!\n");
+
+    let cl_prompt: &str = "(this-repl)> ";
+    let mut stdout = io::stdout().lock();
+
+    'main_loop: loop {
+        write!(stdout, "{}", cl_prompt)?;
+        stdout.flush()?;
+
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+
+        let input_fmt: &str = input.as_str().trim();
+
+        // inspiration from the nix repl for cmd's that begin w/ ':'
+        match input_fmt {
+            ":q" => {
+                writeln!(stdout, "See you next time :)")?;
+                break 'main_loop;
+            }
+            i if i.starts_with(':') => {
+                // let stripped_i = i.strip_prefix(':').unwrap();
+                //
+                // handle special cmd's...
+                // match stripped_i {}
+
+                writeln!(stdout, "You entered: {}", i)?
+            }
+            "" => return Ok(()), // early return if empty
+            i => writeln!(stdout, "You entered: {}", i)?,
+        }
+
+        stdout.flush()?
+    }
+
+    Ok(())
 }
