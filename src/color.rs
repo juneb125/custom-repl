3ce828f23895357e4a11529ae7a0d2b66908fed7
June B <jrbergeron823@gmail.com>
2025-02-14 23:30:57 -0600

    Move ANSI 'dictionary' to documentation comments above the functions

--- src/color.rs
+++ src/color.rs
@@ -6,19 +6,17 @@ pub trait Color {
     fn set_bg(&self, ansi_color: u8) -> String;
 }
 
-/*
- * black: 0, red: 1,     green: 2, yellow: 3,
- * blue: 4,  magenta: 5, cyan: 6,  white: 7
- */
-
 impl<T: Display> Color for T {
+    /// black: 0, red: 1,     green: 2, yellow: 3,
+    /// blue:  4, magenta: 5, cyan:  6, white:  7
     fn set_fg(&self, ansi_color: u8) -> String {
         match ansi_color {
             0..7 => format!("\x1b[3{}m{}\x1b[m", ansi_color, self),
             _ => format!("\x1b[38;5;{}m{}\x1b[m", ansi_color, self),
         }
     }
-
+    /// black: 0, red: 1,     green: 2, yellow: 3,
+    /// blue:  4, magenta: 5, cyan:  6, white:  7
     fn set_bg(&self, ansi_color: u8) -> String {
         match ansi_color {
             0..7 => format!("\x1b[4{}m{}\x1b[m", ansi_color, self),
